ee46e23a74b233ae924a0cb47e1af538ed4e378c
AsmShuvo <hoffmanhulk99@gmail.com>
2025-08-30 23:11:39 +0600

    Add: Bcrypt and hash user pass

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -1,9 +1,12 @@
 import { pool } from "../config/db.js";
+import bcrypt from "bcryptjs";
 
 export async function createUser(username, email, password) {
+  const salt = 10;
+  const passwordHash = await bcrypt.hash(password, salt);
   const [result] = await pool.execute(
     "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-    [username, email, password]
+    [username, email, passwordHash]
   );
   return result.insertId;
 }
